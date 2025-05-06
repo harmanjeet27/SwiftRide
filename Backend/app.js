84778bebe93b49e9895045252f2f9decdfaa907f
harmanjeet27 <27harmanjeet@gmail.com>
2025-05-06 21:03:08 +0530

    server setup and user auth setup

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,23 @@
 const dotenv = require('dotenv'); // Import dotenv to load environment variables
 dotenv.config(); // Load environment variables from .env file 
-
-
 const express = require("express");
 const app = express();
 const cors = require('cors'); // Import CORS middleware
-app.use(cors())
+const cookieParser = require('cookie-parser'); // Import cookie-parser middleware
+const connectToDb = require('./db/db')      
+const userRoutes = require('./routes/user.routes'); // Import user routes
 
+connectToDb();
+
+app.use(cors())
+app.use(express.json()) // Parse JSON request bodies
+app.use(cookieParser()) // Parse cookies from request headers
+app.use(express.urlencoded({ extended: true })) // Parse URL-encoded request bodies
+// dummy route
 app.get('/', (req, res) => {
-    res.send("HEllo ji kaise ho sare ")
+    res.send("HEllo ji kaise ho sare, sab badiya hai ji ")
 })
+
+app.use('/users', userRoutes); // Use user routes for any requests to /users
 
 module.exports = app;
