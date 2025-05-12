897c1a2553723f3be69d8ae191d9a0f5a552e307
harmanjeet27 <27harmanjeet@gmail.com>
2025-05-12 19:29:08 +0530

    Ui Initiated

---
+++ Frontend/src/pages/UserLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserLogin() {
+  return (
+    <div>
+      UserLogin
+    </div>
+  )
+}
+
+export default UserLogin
---
+++ Frontend/src/pages/UserSignup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserSignup() {
+  return (
+    <div>
+      UserSignup
+    </div>
+  )
+}
+
+export default UserSignup
