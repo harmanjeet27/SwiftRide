d1316de7fe053d03989f5a36b7804454dd3dfb46
harmanjeet27 <27harmanjeet@gmail.com>
2025-05-13 18:19:30 +0530

    UI updated and components added

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import CaptainContext from './context/CaptainContext.jsx'
+import UserContext from './context/UserContext.jsx'
+import SocketProvider from './context/SocketContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </StrictMode>,
+  <CaptainContext>
+    <UserContext>
+      <SocketProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SocketProvider>
+    </UserContext>
+  </CaptainContext>,
 )
